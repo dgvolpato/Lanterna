8299cc374051e6c8c40ab018092162bd5a7dbb80
Diego Volpato <dgvlpt@gmail.com>
2014-03-08 03:17:07 -0300

    Adicionado um botão que deverá ligar o flash.

--- src/com/example/app1/MyActivity.java
+++ src/com/example/app1/MyActivity.java
@@ -1,7 +1,12 @@
 package com.example.app1;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +16,26 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+
+    public void myFlash (View view) {
+    /**
+        Context context = this;
+
+        context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        Camera cam = Camera.open();
+        Camera.Parameters p = cam.getParameters();
+        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        cam.setParameters(p);
+        cam.startPreview();
+
+        //cam.stopPreview();
+        //cam.release();
+    */
+        Button flash = (Button) findViewById(R.id.flashPlay);
+        flash.setText("Pisca!!!!");
+
     }
 }
